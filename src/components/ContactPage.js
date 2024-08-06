870c7f8c39453a65f6893b6dedb259c43a250c92
TylerPassyn <Tyler.A.Passyn@gmail.com>
2024-08-06 15:41:15 -0400

    Some Contact Page Stuff, Yipee

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -6,7 +6,7 @@ const ContactPage = () => {
     <div>
       <h1>Contact</h1>
       <p>Get in touch with us!</p>
-      <SiGroupme />
+      <SiGroupme size={20} style={{ color: "#000" }} />
     </div>
   );
 };
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import HeroImage from "../components/HeroImage";
+import ContactPage from "../components/ContactPage";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroImage icon="adress" heading="Contact" text="Get in touch with us!" />
+      <ContactPage />
       <Footer />
     </div>
   );
