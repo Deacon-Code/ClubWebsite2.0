8206f8fdbd6aaedf8dd9a518661c3450c77ffecf
TylerPassyn <Tyler.A.Passyn@gmail.com>
2024-08-06 19:14:17 -0400

    The website is essentially complete
    Just need some specific projects and people now. Then I need to figure out web hosting.

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { SiGroupme } from "react-icons/si";
+import "./ContactPageStyles.css";
+import useRedirectOnClick from "./Hooks/useRedirectOnClick";
 
 const ContactPage = () => {
+  const handleGroupmeClick = useRedirectOnClick(
+    "https://groupme.com/join_group/102163335/5l7p6DwY"
+  );
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>Get in touch with us!</p>
-      <SiGroupme size={20} style={{ color: "#000" }} />
+    <div className="contactContainer">
+      <h1>Join Our GroupMe!</h1>
+      <div className="groupmeLink" onClick={handleGroupmeClick}>
+        <SiGroupme size={200} style={{ color: "rgb(32, 108, 248)" }} />
+      </div>
+      <div className="email">
+        <h1> Club President Email</h1>
+        <p>passta23@wfu.edu</p>
+      </div>
     </div>
   );
 };
